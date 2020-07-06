eaa039c0a2a385f515dfa4d6e25b018a3c3c89fa
jose.mendezgonzalez <jose.mendezgonzalez@plexus.es>
2020-07-06 09:07:57 +0200

    Configurations

--- europair-management-rest/src/main/java/com/europair/management/rest/common/configuration/WebConfiguration.java
+++ europair-management-rest/src/main/java/com/europair/management/rest/common/configuration/WebConfiguration.java
@@ -4,12 +4,14 @@ import org.h2.server.web.WebServlet;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class WebConfiguration {
+    @Profile("dev")
     @Bean
-    ServletRegistrationBean h2servletRegistration(){
-        ServletRegistrationBean registrationBean = new ServletRegistrationBean( new WebServlet());
+    ServletRegistrationBean<WebServlet> h2servletRegistration() {
+        ServletRegistrationBean<WebServlet> registrationBean = new ServletRegistrationBean<>(new WebServlet());
         registrationBean.addUrlMappings("/h2/*");
         return registrationBean;
     }
