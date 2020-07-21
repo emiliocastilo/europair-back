51e2959c89c1e0fd2b83e33df8f357193de947c7
David Castro Aguilar <david.castroaguilar@plexus.es>
2020-07-21 14:03:24 +0100

    context path for external config files

--- europair-management-rest/src/main/java/com/europair/management/rest/EuropairManagementRestApplication.java
+++ europair-management-rest/src/main/java/com/europair/management/rest/EuropairManagementRestApplication.java
@@ -2,7 +2,10 @@ package com.europair.management.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+import java.util.Properties;
 
 @SpringBootApplication
 public class EuropairManagementRestApplication extends SpringBootServletInitializer {
@@ -10,5 +13,19 @@ public class EuropairManagementRestApplication extends SpringBootServletInitiali
 	public static void main(String[] args) {
 		SpringApplication.run(EuropairManagementRestApplication.class, args);
 	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder springApplicationBuilder) {
+		return springApplicationBuilder
+				.sources(EuropairManagementRestApplication.class)
+				.properties(getProperties());
+	}
+
+	static Properties getProperties() {
+		Properties props = new Properties();
+		props.put("spring.config.location", "classpath:europair/");
+		return props;
+	}
+
 
 }
