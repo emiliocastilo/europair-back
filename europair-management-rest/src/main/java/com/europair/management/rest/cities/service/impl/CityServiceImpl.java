8aa5d574b30e3bb5af09cd6ed99ce67608f2b9c1
Juan Sebastian Filippini <juan.sebastianfilippini@plexus.local>
2020-07-21 09:54:17 +0200

    Servicio de ciudades.

---
+++ europair-management-rest/src/main/java/com/europair/management/rest/cities/service/impl/CityServiceImpl.java
@@ -0,0 +1,73 @@
+package com.europair.management.rest.cities.service.impl;
+
+import com.europair.management.rest.cities.repository.CityRepository;
+import com.europair.management.rest.cities.service.CityService;
+import com.europair.management.rest.common.exception.ResourceNotFoundException;
+import com.europair.management.rest.model.cities.dto.CityDTO;
+import com.europair.management.rest.model.cities.entity.City;
+import com.europair.management.rest.model.cities.mapper.CityMapper;
+import com.europair.management.rest.model.countries.dto.CountryDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class CityServiceImpl implements CityService {
+
+    private final CityRepository cityRepository;
+
+    @Override
+    public Page<CityDTO> findAllPaginated(Pageable pageable) {
+        return cityRepository.findAll(pageable).map(city -> CityMapper.INSTANCE.toDto(city));
+    }
+
+    @Override
+    public CityDTO findById(Long id) throws ResourceNotFoundException {
+        return CityMapper.INSTANCE.toDto(cityRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("City not found on id: " + id)));
+    }
+
+    @Override
+    public CityDTO saveCity(final CityDTO cityDTO) {
+        City city = cityRepository.save(CityMapper.INSTANCE.toEntity(cityDTO));
+        return CityMapper.INSTANCE.toDto(city);
+    }
+
+    @Override
+    public CityDTO updateCity(final Long id, final CityDTO cityDTO) {
+
+        City storedCity = cityRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("City not found on id: " + id));
+
+        CityDTO storedCityDTO = CityMapper.INSTANCE.toDto(storedCity);
+
+        CityDTO updatedCityDTO = updateCityValues(storedCityDTO.getId(), cityDTO);
+
+        City city = cityRepository.save(CityMapper.INSTANCE.toEntity(updatedCityDTO));
+
+        return CityMapper.INSTANCE.toDto(city);
+    }
+
+    @Override
+    public void deleteCity(Long id) {
+
+        City city = cityRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("City not found on id: " + id));
+        cityRepository.delete(city);
+    }
+
+    private CityDTO updateCityValues(Long id, CityDTO cityDTO) {
+
+        return CityDTO.builder()
+                .id(id)
+                .code(cityDTO.getCode())
+                .name(cityDTO.getName())
+                .country(CountryDTO.builder().id(cityDTO.getCountry().getId()).build())
+                .build();
+
+    }
+}
--- europair-management-rest/src/main/java/com/europair/management/rest/countries/repository/CountryRepository.java
+++ europair-management-rest/src/main/java/com/europair/management/rest/countries/repository/CountryRepository.java
@@ -4,7 +4,6 @@ import com.europair.management.rest.model.countries.entity.Country;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
