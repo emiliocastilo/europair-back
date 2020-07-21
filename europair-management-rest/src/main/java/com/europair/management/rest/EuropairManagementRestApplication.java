a5d1a5b07a8fb7573502365b6858f0b46cd3aa37
David Castro Aguilar <david.castroaguilar@plexus.es>
2020-07-21 13:06:07 +0100

    WAR artifact from profiles

--- europair-management-rest/src/main/java/com/europair/management/rest/EuropairManagementRestApplication.java
+++ europair-management-rest/src/main/java/com/europair/management/rest/EuropairManagementRestApplication.java
@@ -2,9 +2,10 @@ package com.europair.management.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class EuropairManagementRestApplication {
+public class EuropairManagementRestApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EuropairManagementRestApplication.class, args);
--- europair-management-rest/src/main/java/com/europair/management/rest/common/configuration/SecurityConfiguration.java
+++ europair-management-rest/src/main/java/com/europair/management/rest/common/configuration/SecurityConfiguration.java
@@ -1,18 +1,29 @@
 package com.europair.management.rest.common.configuration;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.util.StringUtils;
+
+import java.util.Arrays;
 
 @Configuration
+@Slf4j
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-    @Profile("dev")
+    @Value("${spring.profiles.active}")
+    private String activeProfile;
+
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.csrf().disable().authorizeRequests().antMatchers("/").permitAll().and().authorizeRequests()
                 .antMatchers("/h2/**").permitAll();
+        if(Arrays.asList(StringUtils.tokenizeToStringArray(activeProfile,",")).contains("dev")){
+            // TODO
+            log.info("Enabling H2 AntMatchers for develop profile");
+        }
         httpSecurity.headers().frameOptions().disable();
     }
 }
