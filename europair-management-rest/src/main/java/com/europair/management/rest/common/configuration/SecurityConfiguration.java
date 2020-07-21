3c92b3ed6264ddeafd0f7400134963d9036ce228
David Castro Aguilar <david.castroaguilar@plexus.es>
2020-07-21 09:47:59 +0100

    + Added OpenAPI and CORS protection

--- europair-management-rest/src/main/java/com/europair/management/rest/common/configuration/SecurityConfiguration.java
+++ europair-management-rest/src/main/java/com/europair/management/rest/common/configuration/SecurityConfiguration.java
@@ -1,18 +1,54 @@
 package com.europair.management.rest.common.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.util.StringUtils;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Value("${cors.origins.allowed}")
+    private String originsAllowed;
 
     @Profile("dev")
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.csrf().disable().authorizeRequests().antMatchers("/").permitAll().and().authorizeRequests()
-                .antMatchers("/h2/**").permitAll();
+                .antMatchers(AUTH_WHITELIST).permitAll();
         httpSecurity.headers().frameOptions().disable();
     }
+
+    private static final String[] AUTH_WHITELIST = {
+            // -- swagger ui
+            "/v3/api-docs",
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "/configuration/ui",
+            "/configuration/security",
+            "/swagger-ui.html",
+            //h2
+            "/h2/**",
+            // other
+            "/error**"
+    };
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList(StringUtils.tokenizeToStringArray(originsAllowed, ";")));
+        configuration.applyPermitDefaultValues();
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
 }
