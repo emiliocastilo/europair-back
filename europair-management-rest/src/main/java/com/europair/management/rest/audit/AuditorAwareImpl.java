6aeb0d2a1d60835cb923fda2f0f46786284ae6a7
David Castro Aguilar <david.castroaguilar@plexus.es>
2020-07-27 09:36:16 +0100

    * Extract author from JWT

--- europair-management-rest/src/main/java/com/europair/management/rest/audit/AuditorAwareImpl.java
+++ europair-management-rest/src/main/java/com/europair/management/rest/audit/AuditorAwareImpl.java
@@ -5,16 +5,24 @@
 package com.europair.management.rest.audit;
 
 import com.europair.management.rest.model.users.dto.UserDTO;
+import com.europair.management.rest.model.users.entity.User;
+import com.europair.management.rest.users.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.util.Optional;
 
 public class AuditorAwareImpl implements AuditorAware<String> {
 
+    @Autowired
+    private UserRepository userRepository;
+
     @Override
     public Optional<String> getCurrentAuditor() {
-        //return Optional.of(personRepo.findByUserPrincipalName(SecurityContextHolder.getContext().getAuthentication().getName()
-        // + "@email.com"));
-        return Optional.of("AUDIT_TEST_USER");
+
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        return userRepository.findByUsername(username).map(u -> u.getName() + " " + u.getSurname());
+        //return Optional.of("AUDIT_TEST_USER");
     }
 }
