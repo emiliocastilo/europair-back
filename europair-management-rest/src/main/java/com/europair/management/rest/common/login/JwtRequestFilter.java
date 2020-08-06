a992d6e04e9b3415f3f3d59f487b9cead0c243ce
ivan <ivan.fanjulcosent@plexus.es>
2020-08-07 01:34:18 +0200

    Migracion al 75%: (cities,countries,roles,screens,tasks,users, conversions(50%))

--- europair-management-rest/src/main/java/com/europair/management/rest/common/login/JwtRequestFilter.java
+++ europair-management-rest/src/main/java/com/europair/management/rest/common/login/JwtRequestFilter.java
@@ -40,6 +40,7 @@ public class JwtRequestFilter extends OncePerRequestFilter {
         // TODO: delete all sout and put this inside a logger
         System.out.println("Unable to get JWT Token");
       } catch (ExpiredJwtException e) {
+        // TODO: delete all sout and put this inside a logger
         System.out.println("JWT Token has expired");
       }
     } else {
