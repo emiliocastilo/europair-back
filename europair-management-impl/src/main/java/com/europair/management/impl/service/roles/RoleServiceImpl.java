819e08d4bd9793e94f2c4353249dffdb1f916817
ivan <ivan.fanjulcosent@plexus.es>
2020-08-14 01:08:06 +0200

    ejemplo de gestion relaciones N:N entre roles y tareas. Solucionado error de edicion pantalla roles

--- europair-management-impl/src/main/java/com/europair/management/impl/service/roles/RoleServiceImpl.java
+++ europair-management-impl/src/main/java/com/europair/management/impl/service/roles/RoleServiceImpl.java
@@ -1,23 +1,44 @@
 package com.europair.management.impl.service.roles;
 
 import com.europair.management.api.dto.roles.RoleDTO;
+import com.europair.management.api.dto.tasks.TaskDTO;
 import com.europair.management.impl.common.exception.InvalidArgumentException;
 import com.europair.management.impl.common.exception.ResourceNotFoundException;
 import com.europair.management.impl.mappers.roles.RoleMapper;
+import com.europair.management.impl.mappers.tasks.TaskMapper;
+import com.europair.management.rest.model.common.repository.BaseRepositoryImpl;
 import com.europair.management.rest.model.roles.entity.Role;
 import com.europair.management.rest.model.roles.repository.IRoleRepository;
+import com.europair.management.rest.model.rolestasks.entity.RolesTasks;
+import com.europair.management.rest.model.rolestasks.entity.RolesTasksPK;
+import com.europair.management.rest.model.rolestasks.repository.RolesTasksRepository;
+import com.europair.management.rest.model.tasks.entity.Task;
+import com.europair.management.rest.model.tasks.repository.ITaskRepositoryCustom;
+import com.europair.management.rest.model.tasks.repository.TaskRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class RoleServiceImpl implements IRoleService {
 
+  private static final Logger logger = LoggerFactory.getLogger(RoleServiceImpl.class);
+
   private final IRoleRepository roleRepository;
+  private final TaskRepository taskRepository;
+  private final RolesTasksRepository rolesTasksRepository;
 
   @Override
   public Page<RoleDTO> findAllPaginated(Pageable pageable) {
@@ -49,11 +70,63 @@ public class RoleServiceImpl implements IRoleService {
     Role role = roleRepository.findById(id)
       .orElseThrow(() -> new ResourceNotFoundException("Role not found on id: " + id));
 
-    RoleMapper.INSTANCE.updateFromDto(roleDTO, role);
-    role = roleRepository.save(role);
+      /*
+      must iterate over the dto list adding all the task that are not in the JPA list
+       */
+      for (TaskDTO taskDTO : roleDTO.getTasks()) {
+        if (!existIdTaskDTOInJPAList(taskDTO, role.getRolesTasks())){
+
+          // we have left hand side of the relationship role but we must have both
+          Task task = taskRepository.findById(taskDTO.getId()).orElseThrow(() -> new ResourceNotFoundException("Task not found on id: " + taskDTO.getId()) );
+
+          RolesTasksPK rolesTasksPK = new RolesTasksPK();
+          rolesTasksPK.setRoleId(role.getId());
+          rolesTasksPK.setTaskId(taskDTO.getId());
+          RolesTasks rolesTasks = new RolesTasks();
+          rolesTasks.setId(rolesTasksPK);
+          rolesTasks.setRole(role);
+          rolesTasks.setTask(task);
+
+          rolesTasksRepository.save(rolesTasks);
+        };
+      }
+      /*
+      mist iterate over the jpa list deleting all the task that are not in the DTO list
+       */
+      for ( RolesTasks rolesTasks : role.getRolesTasks()){
+        if (!existIdTaskInDTOList(rolesTasks, roleDTO.getTasks())){
+          rolesTasksRepository.delete(rolesTasks);
+        }
+      }
+
+    role = roleRepository.findById(id)
+            .orElseThrow(() -> new ResourceNotFoundException("Role not found on id: " + id));
 
     return RoleMapper.INSTANCE.toDto(role);
   }
+
+  private boolean existIdTaskInDTOList(RolesTasks rolesTasks, List<TaskDTO> tasks) {
+    boolean res = false;
+    for (TaskDTO taskDTO : tasks){
+      if (rolesTasks.getId().getTaskId().equals(taskDTO.getId())){
+        res = true;
+        break;
+      }
+    }
+    return res;
+  }
+
+  private boolean existIdTaskDTOInJPAList(TaskDTO taskDTO, Set<RolesTasks> rolesTasks) {
+    boolean res = false;
+    for (RolesTasks roleTask : rolesTasks) {
+      if (taskDTO.getId().equals(roleTask.getId().getTaskId())) {
+        res = true;
+        break;
+      }
+    }
+    return res;
+  }
+
 
   @Transactional(readOnly = false)
   @Override
