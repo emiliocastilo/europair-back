a1ba3b62851fcd10c57762a462954b4c7ac9cb45
jose.mendezgonzalez <jose.mendezgonzalez@plexus.es>
2020-08-20 09:36:21 +0200

    Fix mapping airport modifications

--- europair-management-impl/src/main/java/com/europair/management/impl/mappers/operatorsAirports/IOperatorsAirportsMapper.java
+++ europair-management-impl/src/main/java/com/europair/management/impl/mappers/operatorsAirports/IOperatorsAirportsMapper.java
@@ -4,6 +4,7 @@ import com.europair.management.api.dto.operatorsairports.OperatorsAirportsDTO;
 import com.europair.management.impl.mappers.audit.AuditModificationBaseMapperConfig;
 import com.europair.management.rest.model.operatorsairports.entity.OperatorsAirports;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingInheritanceStrategy;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.factory.Mappers;
@@ -14,10 +15,13 @@ public interface IOperatorsAirportsMapper {
 
   IOperatorsAirportsMapper INSTANCE = Mappers.getMapper(IOperatorsAirportsMapper.class);
 
+  @Mapping(target = "airport.elevation", ignore = true)
   OperatorsAirportsDTO toDto (final OperatorsAirports entity);
 
+  @Mapping(target = "airport.elevation", ignore = true)
   OperatorsAirports toEntity (final OperatorsAirportsDTO operatorsAirportsDTO);
 
+  @Mapping(target = "airport.elevation", ignore = true)
   void updateFromDto(final OperatorsAirportsDTO operatorsAirportsDTO, @MappingTarget OperatorsAirports operatorsAirports);
 
 }
