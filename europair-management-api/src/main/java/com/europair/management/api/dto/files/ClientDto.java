f9099a76c0f08d7b68d3a3cb5b2bc72e4b5fd05e
jose.mendezgonzalez <jose.mendezgonzalez@plexus.es>
2020-09-08 13:29:25 +0200

    added Country, Canary Islands, VIES and type to Clients

--- europair-management-api/src/main/java/com/europair/management/api/dto/files/ClientDto.java
+++ europair-management-api/src/main/java/com/europair/management/api/dto/files/ClientDto.java
@@ -1,13 +1,15 @@
 package com.europair.management.api.dto.files;
 
-
 import com.europair.management.api.dto.audit.AuditModificationBaseDTO;
 import com.europair.management.api.dto.common.TextField;
+import com.europair.management.api.dto.countries.CountryDTO;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
+import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
@@ -28,4 +30,17 @@ public class ClientDto extends AuditModificationBaseDTO {
     @Size(min = 1, max = TextField.TEXT_255)
     @NotEmpty
     private String name;
+
+    @JsonProperty("type")
+    @Size(min = 1, max = TextField.TEXT_20)
+    private String type = "INDIVIDUAL";
+
+    @Column(name = "canary_islands")
+    private Boolean canaryIslands = false;
+
+    @Column(name = "vies")
+    private Boolean vies = false;
+
+    @ManyToOne
+    private CountryDTO country;
 }
