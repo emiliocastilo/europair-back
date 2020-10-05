8cd121c6938242e1ead37770387363f70631e81c
ivan <ivan.fanjulcosent@plexus.es>
2020-10-05 14:57:17 +0200

    refactor PlanningServiceImpl

--- europair-management-impl/src/main/java/com/europair/management/impl/integrations/office365/planning/IPlanningService.java
+++ europair-management-impl/src/main/java/com/europair/management/impl/integrations/office365/planning/IPlanningService.java
@@ -1,11 +1,16 @@
 package com.europair.management.impl.integrations.office365.planning;
 
 import com.europair.management.api.integrations.office365.dto.PlanningFlightsDTO;
+import com.europair.management.rest.model.files.entity.File;
+import com.europair.management.rest.model.flights.entity.Flight;
+import com.europair.management.rest.model.routes.entity.Route;
 
 import java.util.List;
 
 public interface IPlanningService {
 
     List<PlanningFlightsDTO> getPlanningFlightsInfo(Long routeId, String actionType);
+
+    PlanningFlightsDTO getPlanningFlightsDTO(String actionType, Route route, File file, Flight flight);
 
 }
