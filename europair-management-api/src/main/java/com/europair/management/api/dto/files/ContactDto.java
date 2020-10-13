20b33593b0225c80e488e47730f381d92fb96604
axel.sanchez <axel.sanchezsalom@plexus.es>
2020-10-13 16:39:54 +0200

    Updated contact entity, dto and table

--- europair-management-api/src/main/java/com/europair/management/api/dto/files/ContactDto.java
+++ europair-management-api/src/main/java/com/europair/management/api/dto/files/ContactDto.java
@@ -2,7 +2,10 @@ package com.europair.management.api.dto.files;
 
 
 import com.europair.management.api.dto.audit.AuditModificationBaseDTO;
+import com.europair.management.api.dto.cities.CityDTO;
 import com.europair.management.api.dto.common.TextField;
+import com.europair.management.api.dto.countries.CountryDTO;
+import com.europair.management.api.enums.OperationTypeEnum;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -28,4 +31,52 @@ public class ContactDto extends AuditModificationBaseDTO {
     @Size(min = 1, max = TextField.TEXT_255)
     @NotEmpty
     private String name;
+
+    @NotEmpty
+    @Size(min = 1, max = TextField.TEXT_30)
+    private String crmCode;
+
+    @Size(max = TextField.TEXT_20)
+    private String contactType;
+
+    @Size(max = TextField.TEXT_30)
+    private String companyCode;
+
+    private String companyName;
+
+    @Size(max = TextField.TEXT_20)
+    private String contactCategory; // ToDo: enum?? valores: proveedor aéreo, operador, cliente, proveedor servicios generales, bróker, agente de handling y otros
+
+    private String alias;
+
+    private String address;
+
+    private Long cityId;
+    private CityDTO city;
+
+    @Size(max = TextField.TEXT_20)
+    private String postalCode;
+
+    @Size(max = TextField.TEXT_120)
+    private String province;
+
+    private Long countryId;
+    private CountryDTO country;
+
+    @Size(max = TextField.TEXT_30)
+    private OperationTypeEnum commonOperationType;
+
+    private String email;
+
+    @Size(max = TextField.TEXT_40)
+    private String phoneNumber;
+
+    @Size(max = TextField.TEXT_30)
+    private String mobilePhoneNumber;
+
+    @Size(max = TextField.TEXT_30)
+    private String faxNumber;
+
+    private String observation;
+
 }
