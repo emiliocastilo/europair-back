d31855b6791e1f26cd265cb097454348a7faed27
ivan <ivan.fanjulcosent@plexus.es>
2020-10-29 18:32:39 +0100

    activar logs en intergracion

--- europair-management-rest/src/main/java/com/europair/management/rest/audit/AuditorAwareImpl.java
+++ europair-management-rest/src/main/java/com/europair/management/rest/audit/AuditorAwareImpl.java
@@ -4,7 +4,10 @@
 
 package com.europair.management.rest.audit;
 
+import com.europair.management.rest.common.configuration.JWTAuthenticationFilter;
 import com.europair.management.rest.model.users.repository.IUserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -19,6 +22,8 @@ public class AuditorAwareImpl implements AuditorAware<String> {
     @Autowired
     private IUserRepository userRepository;
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(AuditorAwareImpl.class);
+
 
     @Override
     public Optional<String> getCurrentAuditor() {
@@ -27,12 +32,27 @@ public class AuditorAwareImpl implements AuditorAware<String> {
 
         // Audit for External users
         String username = SecurityContextHolder.getContext().getAuthentication().getName();
+
+        LOGGER.debug("AuditorAwareImpl :  username -> " + username);
+
         res = userRepository.findByUsername(username).map(u -> u.getUsername());
 
         // Audit for Azure users
         if ( res.isEmpty()) {
+
+            LOGGER.debug("AuditorAwareImpl intentando recuperar usuario de azure : ");
+
             String emailAzure = (String) ((Jwt) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getClaims().get("unique_name");
+
+            LOGGER.debug("AuditorAwareImpl intentando recuperar usuario de azure con email : " + emailAzure);
+
             Optional <String> azureUserName = userRepository.findByEmail(emailAzure).map(u -> u.getUsername());
+
+            if ( azureUserName.isEmpty()){
+                LOGGER.debug("AuditorAwareImpl NO RECUPERADO USER DE AZURE");
+            }
+            LOGGER.debug("tanto si encuentra como si no:" + azureUserName.get());
+
             res= azureUserName;
         }
 
