f3e043ad8dc14479f95394a5855fe92bb12b7f1b
axel.sanchez <axel.sanchezsalom@plexus.es>
2020-11-03 14:58:01 +0100

    Organize imports

--- europair-management-api/src/main/java/com/europair/management/api/dto/contribution/ContributionDTO.java
+++ europair-management-api/src/main/java/com/europair/management/api/dto/contribution/ContributionDTO.java
@@ -16,7 +16,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
