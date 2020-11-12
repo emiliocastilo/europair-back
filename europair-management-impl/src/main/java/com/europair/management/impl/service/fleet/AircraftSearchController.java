f67cd03038d8aeaee6e71fc2762008c504087436
axel.sanchez <axel.sanchezsalom@plexus.es>
2020-11-12 13:06:02 +0100

    Fix aircraft search near airports and add log to controller

--- europair-management-impl/src/main/java/com/europair/management/impl/service/fleet/AircraftSearchController.java
+++ europair-management-impl/src/main/java/com/europair/management/impl/service/fleet/AircraftSearchController.java
@@ -4,6 +4,8 @@ import com.europair.management.api.dto.fleet.AircraftFilterDto;
 import com.europair.management.api.dto.fleet.AircraftSearchResultDataDto;
 import com.europair.management.api.service.fleet.IAircraftSearchController;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,12 +16,17 @@ import java.util.List;
 @Slf4j
 public class AircraftSearchController implements IAircraftSearchController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(AircraftSearchController.class);
+
     @Autowired
     private IAircraftSearchService aircraftSearchService;
 
     @Override
     public ResponseEntity<List<AircraftSearchResultDataDto>> searchAircraft(AircraftFilterDto filterDto) {
-        return ResponseEntity.ok(aircraftSearchService.searchAircraft(filterDto));
+        LOGGER.debug("[AircraftSearchController] - Starting method [searchAircraft] with input: filterDto={}", filterDto);
+        List<AircraftSearchResultDataDto> res = aircraftSearchService.searchAircraft(filterDto);
+        LOGGER.debug("[AircraftSearchController] - Ending method [searchAircraft] with return: {}", res);
+        return ResponseEntity.ok(res);
     }
 
 }
