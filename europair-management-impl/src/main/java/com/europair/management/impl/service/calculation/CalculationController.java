6cff4c563a44759bfd990b377e9a82574f5b36b2
axel.sanchez <axel.sanchezsalom@plexus.es>
2020-12-04 12:30:20 +0100

    CalculationController logging

--- europair-management-impl/src/main/java/com/europair/management/impl/service/calculation/CalculationController.java
+++ europair-management-impl/src/main/java/com/europair/management/impl/service/calculation/CalculationController.java
@@ -25,33 +25,40 @@ public class CalculationController implements ICalculationController {
 
     @Override
     public ResponseEntity<VatCalculationResponseDto> calculateVat(@NotNull @Valid VatCalculationRequestDto vatCalculationRequestDto) {
-        Pair<Double, Double> result = calculationService.calculateTaxToApplyAndPercentage(
+        LOGGER.debug("[CalculationController] - Starting method [calculateVat] with input: vatCalculationRequestDto={}", vatCalculationRequestDto);
+        Pair<Double, Double> doublePair = calculationService.calculateTaxToApplyAndPercentage(
                 vatCalculationRequestDto.getFileId(),
                 vatCalculationRequestDto.getOriginAirportId(),
                 vatCalculationRequestDto.getDestinationAirportId(),
                 vatCalculationRequestDto.getServiceType(),
                 vatCalculationRequestDto.isSale());
-        return ResponseEntity.ok(new VatCalculationResponseDto(result.getLeft(), result.getRight()));
+        VatCalculationResponseDto result = new VatCalculationResponseDto(doublePair.getLeft(), doublePair.getRight());
+        LOGGER.debug("[CalculationController] - Ending method [calculateVat] with return: {}", result);
+        return ResponseEntity.ok(result);
     }
 
     @Override
     public ResponseEntity<Double> calculateVatPercentage(@NotNull @Valid VatCalculationRequestDto vatCalculationRequestDto) {
+        LOGGER.debug("[CalculationController] - Starting method [calculateVatPercentage] with input: vatCalculationRequestDto={}", vatCalculationRequestDto);
         Double result = calculationService.calculateServiceTaxToApply(
                 vatCalculationRequestDto.getFileId(),
                 vatCalculationRequestDto.getOriginAirportId(),
                 vatCalculationRequestDto.getDestinationAirportId(),
                 vatCalculationRequestDto.getServiceType(),
                 vatCalculationRequestDto.isSale());
+        LOGGER.debug("[CalculationController] - Ending method [calculateVatPercentage] with return: {}", result);
         return ResponseEntity.ok(result);
     }
 
     @Override
     public ResponseEntity<Double> calculateVatRoutePercentage(@NotNull @Valid VatCalculationRequestDto vatCalculationRequestDto) {
+        LOGGER.debug("[CalculationController] - Starting method [calculateVatRoutePercentage] with input: vatCalculationRequestDto={}", vatCalculationRequestDto);
         Double result = calculationService.calculatePercentageOfTaxToApply(
                 vatCalculationRequestDto.getOriginAirportId(),
                 vatCalculationRequestDto.getDestinationAirportId(),
                 vatCalculationRequestDto.getServiceType(),
                 vatCalculationRequestDto.isSale());
+        LOGGER.debug("[CalculationController] - Ending method [calculateVatRoutePercentage] with return: {}", result);
         return ResponseEntity.ok(result);
     }
 }
